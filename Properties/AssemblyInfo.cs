16eb59ac0ae06ae95ab6225e1fc1e996c66bbe57
WongKit <wongkit@web.de>
2021-02-03 10:10:01 +0100

    Added permissive license "GNU GPLv3" along with copyright notices.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FT Launcher")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCopyright("Copyright © 2021 WongKit")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- src/FormMain.Designer.cs
+++ src/FormMain.Designer.cs
@@ -1,4 +1,22 @@
-﻿namespace FT_Launcher
+﻿/* 
+ * FT Launcher - a simple and robust game updater/launcher
+ * Copyright (C) 2021 WongKit
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace FT_Launcher
 {
     partial class FormMain
     {
